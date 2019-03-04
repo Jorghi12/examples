e812bc8603fc10c24906b5eb0e9de8401ba2edc7
Jorg Doku <jd728@cornell.edu>
2019-03-04 10:54:33 -0500

    Separate stages for adding words and tokenizing.
    All words should be added to the dictionary before tokenizing. This is to ensure that tokenize() has the same self.dictionary.word2idx.

--- word_language_model/data.py
+++ word_language_model/data.py
@@ -15,7 +15,8 @@ class Dictionary(object):
         else:
             self.wordfreq[word] +=1
 
-    def setup_ids(self):
+    def reset_ids(self):
+        """ Set the ids of the words based on decreasing frequency"""
         self.idx2word = []
         self.word2idx = {}
 
@@ -33,33 +34,44 @@ class Dictionary(object):
 class Corpus(object):
     def __init__(self, path):
         self.dictionary = Dictionary()
+
+        # Add the words to the dictionary
+        self.add_words(
+            [os.path.join(path, 'train.txt'),
+            os.path.join(path, 'valid.txt'),
+            os.path.join(path, 'test.txt')])
+
+        # Tokenize the words
         self.train = self.tokenize(os.path.join(path, 'train.txt'))
         self.valid = self.tokenize(os.path.join(path, 'valid.txt'))
         self.test = self.tokenize(os.path.join(path, 'test.txt'))
 
+    def add_words(self, paths=[]):
+        """Add words to dictionary"""
+        for path in paths:
+            assert os.path.exists(path)
+            with open(path, 'r', encoding="utf8") as f:
+                tokens = 0
+                for line in f:
+                    words = line.split() + ['<eos>']
+                    tokens += len(words)
+                    for word in words:
+                        self.dictionary.add_word(word)
+
+        # Set the ids for the words
+        self.dictionary.reset_ids()
+
     def tokenize(self, path):
         """Tokenizes a text file."""
         assert os.path.exists(path)
-        # Add words to the dictionary
-        with open(path, 'r', encoding="utf8") as f:
-            tokens = 0
-            for line in f:
-                words = line.split() + ['<eos>']
-                tokens += len(words)
-                for word in words:
-                    self.dictionary.add_word(word)
 
-        # Update the ids. Needs to be called after new words are added.
-        self.dictionary.setup_ids()
-        
-        # Tokenize file content
         with open(path, 'r', encoding="utf8") as f:
-            ids = torch.LongTensor(tokens)
-            token = 0
+            ids = [] #torch.LongTensor(tokens)
             for line in f:
                 words = line.split() + ['<eos>']
                 for word in words:
-                    ids[token] = self.dictionary.word2idx[word]
-                    token += 1
+                    ids.append(self.dictionary.word2idx[word])
+        # Transform list to Tensor
+        ids = torch.LongTensor(ids)
 
         return ids
