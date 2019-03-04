9c1248a081e9b898d379284ec034832c886da32f
Jorg Doku <jd728@cornell.edu>
2019-03-04 09:01:40 -0500

    IDX by decreasing frequency
    This version supports sorting the indices of words based upon word frequency count.

--- word_language_model/data.py
+++ word_language_model/data.py
@@ -6,12 +6,30 @@ class Dictionary(object):
     def __init__(self):
         self.word2idx = {}
         self.idx2word = []
+        self.wordfreq = {}
 
     def add_word(self, word):
-        if word not in self.word2idx:
-            self.idx2word.append(word)
-            self.word2idx[word] = len(self.idx2word) - 1
+        # Store the word frequencies
+        if word not in self.wordfreq:
+            self.wordfreq[word] = 1
+        else:
+            self.wordfreq[word] +=1
+
+        # Update the word ids.
+        self.setup_ids()
+
         return self.word2idx[word]
+
+    def setup_ids(self):
+        self.idx2word = []
+        self.word2idx = {}
+
+        # Store words by descending frequency
+        self.idx2word = sorted(self.wordfreq, key=self.wordfreq.get, reverse=True)
+
+        # Store the word to idx
+        for idx, word in enumerate(self.idx2word):
+            self.word2idx[word] = idx
 
     def __len__(self):
         return len(self.idx2word)
